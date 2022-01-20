c9b346bf51eea59fc052bc6374023f8da543335c
Alaina <alaina.gossett@gmail.com>
2022-01-19 17:54:43 -0700

    adds API info

--- src/App.js
+++ src/App.js
@@ -8,7 +8,22 @@ import './index.css';
 import Form from './components/Form';
 import ParkDisplay from './components/ParkDisplay';
 
+
 function App() {
+
+  const API_KEY = process.env.REACT_APP_API_KEY
+  const [park, setPark] = useState(null);
+
+  const getPark = async (state) => {
+    //make fetch request and store response
+    const response = await fetch(
+      `https://developer.nps.gov/api/v1/parks?stateCode=${state}&api_key=${API_KEY}`
+    );
+    const data = await response.json();
+
+    setPark(data);
+  }
+
   return (
     <div className="App">
      <Form />
