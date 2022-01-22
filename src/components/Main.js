14671b9227f29129019b7996896489633c039094
Alaina <alaina.gossett@gmail.com>
2022-01-22 14:02:03 -0700

    adds US state dropdown list

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,28 +11,39 @@ const Main = (props) => {
   const [inputState, setInputState] = useState("");
   
   const handleClick = async () => {
-    if(inputState.length < 2) return;
-    //if length is less than state abbreviation, return
-    const URL = 'http://localhost:3001/parks/search?stateCode='
-    const response = await fetch(URL + inputState);
-    const data = await response.json();
-    setParkState(data);
-    // console.log(parkState)
-    setInputState("");
-  }
-
-  const handleChange = (s) => {
-      setInputState(s.target.value);
-  }
-
+      if(inputState.length < 2) return;
+      //if length is less than state abbreviation, return
+      const URL = 'http://localhost:3001/parks/search?stateCode='
+      const response = await fetch(URL + inputState);
+      const data = await response.json();
+      setParkState(data);
+      // console.log(parkState)
+      setInputState("");
+    }
+    
+    const handleChange = (s) => {
+        setInputState(s.target.value);
+    }
+    
+    const statesList=["AK","AL","AZ","AR","CA","CO","CT","DE","FL","GA","HI","ID","IL","IN","IA","KS","KY",
+      "LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR",
+      "PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
     return (
         <>
         <div className='search'>
-                <input 
+            <select name="stateSel" value={inputState} onChange={handleChange}>
+                <option value="">Select State</option>
+                {
+                    statesList.map((s) => {
+                       return <option value={s}>{s}</option>
+                    })
+                }
+            </select>
+                {/* <input 
                 type="text"
                 placeholder="AL"
                 value={inputState}
-                onChange={handleChange} />
+                onChange={handleChange} /> */}
                 <button onClick={handleClick}>Search</button>
         </div>
             { parkState ?
